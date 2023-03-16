fe6095507edfb8bb826e5e98a0f98e2e5aabfe6f
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-03-17 01:09:58 +0530

    Remove __ prefix from member variables in MainWindow

--- src/ui_py/main_window.py
+++ src/ui_py/main_window.py
@@ -1,8 +1,9 @@
 from PySide6.QtWidgets import QMainWindow
 from ui_py.root_widget import RootWidget
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.__root_widget = RootWidget()
-        self.setCentralWidget(self.__root_widget)
+        self.root_widget = RootWidget()
+        self.setCentralWidget(self.root_widget)
