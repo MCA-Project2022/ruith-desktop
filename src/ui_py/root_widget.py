4f96ccdf578e2a32b51b539fa71d16f6f60a51db
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-03-18 23:53:25 +0530

    Add key shortcuts for buttons and combo box

--- src/ui_py/root_widget.py
+++ src/ui_py/root_widget.py
@@ -1,5 +1,6 @@
 from generated_py.ui_root_widget import Ui_root_widget
 from PySide6.QtWidgets import QWidget, QFileDialog, QMessageBox
+from PySide6.QtGui import QKeySequence, QShortcut
 from utilities.document_converter import DocumentConverter
 from utilities.document_format import DocumentFormat
 from pathlib import Path
@@ -14,6 +15,7 @@ class RootWidget(QWidget, Ui_root_widget):
         self.setupUi(self)
         self.loading_widget = LoadingWidget()
         self.setup_slots()
+        self.setup_key_shortcuts()
 
     def setup_slots(self):
         self.input_doc_browse_btn.clicked.connect(self.select_input_file)
@@ -22,9 +24,21 @@ class RootWidget(QWidget, Ui_root_widget):
             self.set_input_file_format)
         self.output_doc_format_cb.currentIndexChanged.connect(
             self.set_output_file_format)
-        self.convert_btn.clicked.connect(self.convert_btn_clicked_slot)
+        self.convert_btn.clicked.connect(self.start_conversion)
         self.doc_converter.succeeded.connect(self.show_success_msg)
         self.doc_converter.failed.connect(self.show_failure_msg)
+
+    def setup_key_shortcuts(self):
+        QShortcut(QKeySequence("Ctrl+i"),
+                  self).activated.connect(self.input_doc_format_cb.showPopup)
+        QShortcut(QKeySequence("Ctrl+o"),
+                  self).activated.connect(self.output_doc_format_cb.showPopup)
+        QShortcut(QKeySequence("i"),
+                  self).activated.connect(self.select_input_file)
+        QShortcut(QKeySequence("o"),
+                  self).activated.connect(self.set_output_file)
+        QShortcut(QKeySequence("c"),
+                  self).activated.connect(self.start_conversion)
 
     def set_input_file_format(self):
         self.doc_converter.input_format = DocumentFormat.int_to_DocumentFormat(
@@ -53,13 +67,16 @@ class RootWidget(QWidget, Ui_root_widget):
     def show_success_msg(self):
         self.loading_widget.hide()
         QMessageBox.information(
-            self, 'Success', f'''Document was successfully converted from {self.doc_converter.input_format.value} to {self.doc_converter.output_format.value}''')
+            self, 'Success',
+            f'''Document was successfully converted from {
+                self.doc_converter.input_format.value} to {
+                    self.doc_converter.output_format.value}''')
 
     def show_failure_msg(self):
         self.loading_widget.hide()
         QMessageBox.critical(
             self, 'Failure', 'Something went wrong please try again.')
 
-    def convert_btn_clicked_slot(self):
+    def start_conversion(self):
         self.loading_widget.show()
         self.doc_converter.start()
